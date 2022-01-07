760601e421f22fa4d0b557f24b036087c71f989c
thorgull <thorgull@gmail.com>
2022-01-07 22:51:52 +0100

    Generalize tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,36 +2,93 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] {
-            new Item("foo", 0, 0),
-            new Item("foo", 1, 1),
-            new Item("foo", 10, 20),
-            new Item("foo", -1, 0),
-            new Item("foo", -1, 5),
-        };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertItemEquals("foo", -1, 0, app.items[0]);
-        assertItemEquals("foo", 0, 0, app.items[1]);
-        assertItemEquals("foo", 9, 19, app.items[2]);
-        assertItemEquals("foo", -2, 0, app.items[3]);
-        assertItemEquals("foo", -2, 3, app.items[4]);
+    static class TestCase {
+        final String name;
+        final int initialSellIn;
+        final int initialQuality;
+        final int expectedSellIn;
+        final int expectedQuality;
+
+        public TestCase(String name, int initialSellIn, int initialQuality, int expectedSellIn, int expectedQuality) {
+            this.name = name;
+            this.initialSellIn = initialSellIn;
+            this.initialQuality = initialQuality;
+            this.expectedSellIn = expectedSellIn;
+            this.expectedQuality = expectedQuality;
+        }
+
+        public Item createItem() {
+            return new Item(name, initialSellIn, initialQuality);
+        }
+
+        public void assertCorrect(Item item) {
+            assertAll(
+                "Failed test case : " + this,
+                () -> assertEquals(name, item.name, "invalid name"),
+                () -> assertEquals(expectedSellIn, item.sellIn, "invalid sellIn"),
+                () -> assertEquals(expectedQuality, item.quality, "invalid quantity")
+            );
+        }
+
+        @Override
+        public String toString() {
+            return "TestCase{" +
+                "name='" + name + '\'' +
+                ", initialSellIn=" + initialSellIn +
+                ", initialQuality=" + initialQuality +
+                ", expectedSellIn=" + expectedSellIn +
+                ", expectedQuality=" + expectedQuality +
+                '}';
+        }
     }
 
-    static void assertItemEquals(String name, int sellIn, int quality, Item item) {
+    void genericTest(TestCase... testCases) {
+        Item[] items = Arrays.stream(testCases).map(TestCase::createItem).toArray(Item[]::new);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(testCases.length, app.items.length);
+
         Assertions.assertAll(
-            () -> assertEquals(name, item.name, "invalid name"),
-            () -> assertEquals(sellIn, item.sellIn, "invalid sellIn"),
-            () -> assertEquals(quality, item.quality, "invalid quantity")
+            IntStream.range(0, testCases.length)
+                .<org.junit.jupiter.api.function.Executable>mapToObj((index) ->
+                    () -> testCases[index].assertCorrect(app.items[index])
+                ).toArray(Executable[]::new)
         );
     }
+
+    @Test
+    void foo() {
+        genericTest(
+            new TestCase("foo", 0, 0, -1, 0),
+            new TestCase("foo", 1, 1, 0, 0),
+            new TestCase("foo", 10, 20, 9, 19),
+            new TestCase("foo", -1, 0, -2, 0),
+            new TestCase("foo", -1, 1, -2, 0),
+            new TestCase("foo", -1, 5, -2, 3)
+        );
+    }
+
+    @Test
+    void testSulfuras() {
+        for (int q = 0; q < 80; q++) {
+            for (int d = -10; d <= 10; d++)
+            genericTest(
+                new TestCase("Sulfuras, Hand of Ragnaros", d, q, d, q)
+            );
+        }
+    }
+
 
     static String toString(Item item) {
         return "Item { name: " + item.name + ", sellIn: " + item.sellIn + ", quality: "+ item.quality + "}";
