f2eaf8606032ead48b29fcb5b07524f8ccac1d11
thorgull <thorgull@gmail.com>
2022-01-07 22:07:21 +0100

    enhance existing test.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,16 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertItemEquals("foo", -1, 0, app.items[0]);
     }
 
+    static void assertItemEquals(String name, int sellIn, int quality, Item item) {
+        assertEquals(name, item.name);
+        assertEquals(sellIn, item.sellIn);
+        assertEquals(quality, item.quality);
+    }
+
+    static String toString(Item item) {
+        return "Item { name: " + item.name + ", sellIn: " + item.sellIn + ", quality: "+ item.quality + "}";
+    }
 }
