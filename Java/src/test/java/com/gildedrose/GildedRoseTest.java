697adbef7811592e410cd984c5af4e622ad0f801
thorgull <thorgull@gmail.com>
2022-01-07 22:14:09 +0100

    enhance existing test.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,16 +9,28 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] {
+            new Item("foo", 0, 0),
+            new Item("foo", 1, 1),
+            new Item("foo", 10, 20),
+            new Item("foo", -1, 0),
+            new Item("foo", -1, 5),
+        };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertItemEquals("foo", -1, 0, app.items[0]);
+        assertItemEquals("foo", 0, 0, app.items[1]);
+        assertItemEquals("foo", 9, 19, app.items[2]);
+        assertItemEquals("foo", -2, 0, app.items[3]);
+        assertItemEquals("foo", -2, 3, app.items[4]);
     }
 
     static void assertItemEquals(String name, int sellIn, int quality, Item item) {
-        assertEquals(name, item.name);
-        assertEquals(sellIn, item.sellIn);
-        assertEquals(quality, item.quality);
+        Assertions.assertAll(
+            () -> assertEquals(name, item.name, "invalid name"),
+            () -> assertEquals(sellIn, item.sellIn, "invalid sellIn"),
+            () -> assertEquals(quality, item.quality, "invalid quantity")
+        );
     }
 
     static String toString(Item item) {
